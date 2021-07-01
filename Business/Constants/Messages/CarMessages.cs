984d885169e0753e14cbc1316dec17eebe1300da
KozZzy <mehmetcekirdekci@outlook.com>
2021-07-02 02:36:06 +0300

    The system of JWT installed

--- Business/Constants/Messages/CarMessages.cs
+++ Business/Constants/Messages/CarMessages.cs
@@ -12,6 +12,12 @@ namespace Business.Constants.Messages
         public static string CarNameInvalid = "Car Name is Invalid.";
         public static string CarPriceInvalid = "Car Price is Invalid.";
         public static string CarsListed = "Cars Listed.";
-
+        public static string AuthorizationDenied = "Authorization Denied.";
+        public static string UserRegistered = "User Registered.";
+        public static string UserNotFound = "User Not Found.";
+        public static string PasswordError = "Password Error.";
+        public static string SuccessfulLogin = "Successful Login.";
+        public static string UserAlreadyExists = "User Already Exists.";
+        public static string AccessTokenCreated = "Access Token Created.";
     }
 }
--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -4,16 +4,14 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    class ErrorDataResult<T> : DataResult<T>
+    public class ErrorDataResult<T> : DataResult<T>
     {
         public ErrorDataResult(T data, string message) : base(data, false, message)
         {
-
         }
 
         public ErrorDataResult(T data) : base(data, false)
         {
-
         }
 
         public ErrorDataResult(string message) : base(default, false, message)
